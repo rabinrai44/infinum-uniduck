fd88dbc5d24d233cf82c56e695286a3e36c34ce7
Rabin Rai <rabinrai44@gmail.com>
2019-07-05 18:41:26 -0400

    Apply button for link based on the parent list class name exist using JS.

--- backend/uniduck/wp-content/themes/uniduck/Assets/JS/main.js
+++ backend/uniduck/wp-content/themes/uniduck/Assets/JS/main.js
@@ -6,3 +6,20 @@ menuIcon.addEventListener('click', () => {
   menuIcon.classList.toggle('menu-icon-close-x');
   navbar.classList.toggle('show-navbar-nav');
 });
+
+// Function being called
+addButtonClass();
+
+function addButtonClass() {
+  // Add a special class name for the nav link if condition is true
+  const navItemIos = document.querySelector('.apply-button-ios');
+  const navItemUnicorn = document.querySelector('.apply-button-unicorn');
+
+  // .btn-primary-ios
+  const navItemA = navItemIos.getElementsByTagName('a')[0];
+  navItemA.className += 'btn btn-primary icon-ios';
+
+  // .btn-secondary-unicorn
+  const navItemU = navItemUnicorn.getElementsByTagName('a')[0];
+  navItemU.className += 'btn btn-secondary icon-unicorn';
+}
