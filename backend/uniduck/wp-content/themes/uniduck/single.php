f5ad096a68508c8ebba5e7d564c385a4fce4064f
Rabin Rai <rabinrai44@gmail.com>
2019-07-07 18:00:00 -0400

    Background image fallback for the blog-header.

--- backend/uniduck/wp-content/themes/uniduck/single.php
+++ backend/uniduck/wp-content/themes/uniduck/single.php
@@ -1,12 +1,39 @@
-<?php get_header(); ?>
+<?php 
+
+/** 
+ * Single Post Template
+*/
+
+$thumbnail_url = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+ get_header(); 
+ 
+ ?>
+
 
     <?php 
     
     if(have_posts());
         while(have_posts()) {
             the_post();
-    ?>
-    
+    // check if there is post thumbnail attached
+    if (has_post_thumbnail()) { ?>
+
+    <header class="blog-header" 
+        style="background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+ url('<?php echo $thumbnail_url; ?>')">
+        <div class="container flex-item-column blog-header-content text-center">
+            <h1 class="blog-title">
+            <?php the_title(); ?>
+            </h1>
+            <div class="blog-author"><span class="dashicons-before dashicons-admin-users"></span> <?php the_author(); ?></div>
+        </div>
+        <div class="back-to-blog">
+            <a href="<?php echo site_url(''); ?>">Back to blog</a>
+        </div>
+        </header>
+        <?php 
+
+    } else { // fallback ?>
     <header class="blog-header">
       <div class="container flex-item-column blog-header-content text-center">
         <h1 class="blog-title">
@@ -18,7 +45,7 @@
         <a href="<?php echo site_url(''); ?>">Back to blog</a>
       </div>
     </header>
-
+    <?php } ?>
     <div class="container container-narrow flex-item-column">
         
         <div class="blog-single">
